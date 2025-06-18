e063b8d74d9c11afb057999b2ec704ac5564bbfb
rexone999 <projectmajorvd25@gmail.com>
2025-06-18 15:24:02 +0530

    atls-connect

---
+++ next.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: 'frame-ancestors *',
+          },
+        ],
+      },
+    ];
+  },
+}; 
